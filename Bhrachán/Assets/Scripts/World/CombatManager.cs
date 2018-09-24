ae43ab156bd82ed2568ed7af479449b852f17e55
ThatBirkl <lkbirkl@googlemail.com>
2018-09-24 22:40:32 +0200

    Started working on combat

---
+++ Bhrachán/Assets/Scripts/World/CombatManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    static List<b_ActorCombat> actors;
+
+	// Use this for initialization
+	void Start ()
+    {
+        actors = new List<b_ActorCombat>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public static void AddActor(b_ActorCombat _actor, int _sec)
+    {
+        bool added = false;
+
+        if (actors.Count == 0)
+        {
+            actors.Add(_actor);
+        }
+        else
+        {
+            foreach (b_ActorCombat a in actors)
+            {
+                if (a.SecondsToWait > _sec)
+                {
+                    if (actors.IndexOf(a) == 0)
+                    {
+                        actors.Insert(0, _actor);
+                    }
+                    else
+                    {
+                        actors.Insert(actors.IndexOf(a) - 1, _actor);
+                    }
+                    added = true;
+                    break;
+                }
+            }
+
+            if (!added)
+            {
+                actors.Add(_actor);
+            }
+        }
+    }
+
+    public static void NextTurn()
+    {
+        actors[0].MakeTurn();
+
+        actors.RemoveAt(0);
+    }
+
+    public static void EndCombat()
+    {
+        actors = null;
+    }
+}
---
+++ Bhrachán/Assets/Scripts/World/b_ActorCombat.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b_ActorCombat : b_WorldObject
+{
+    bool active;
+    b_Character character;
+
+    int secondsToWait;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        active = false;
+	}
+	
+	// Update is called once per frame
+	protected override void Update ()
+    {
+        if (!active)
+            return;
+
+
+        base.Update();
+	}
+
+    //-------------------------------------------
+    public virtual void SwitchToCombat(b_Character _character)
+    {
+        //saftey measure
+        if (active)
+            return;
+
+        character = _character;
+    }
+
+    public virtual void SwitchToWorld()
+    {
+        //safety measure
+        if (!active)
+            return;
+
+        active = false;
+        GetComponent<b_ActorWorldBase>().SwitchToWorld(character);
+    }
+
+    public virtual void MakeTurn()
+    {
+        //other stuff
+        CombatManager.AddActor(this, secondsToWait);
+        CombatManager.NextTurn();
+    }
+
+    //------------------ GETTERS ---------------
+
+    public int SecondsToWait
+    {
+        get { return secondsToWait; }
+    }
+}
