f5212cff8f62c03188e3d5ca2f7e1a5a9b5c3236
ThatBirkl <lkbirkl@googlemail.com>
2018-10-01 21:56:00 +0200

    MultiText

---
+++ Bhrachán/Assets/Scripts/UI/b_UIText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b_UIText : MonoBehaviour
+{
+	protected virtual void Update ()
+    {
+        Close();	
+	}
+
+    private void Close()
+    {
+        if (Input.GetAxis("Cancel") != 0)
+        {
+            Destroy(gameObject);
+        }       
+    }
+}
---
+++ Bhrachán/Assets/Scripts/UI/b_UITextMultipage.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b_UITextMultipage : b_UIText
+{
+    ArrayList text;
+    int displayPointer;
+
+    void Start()
+    {
+        displayPointer = 0;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        SwitchPage();
+    }
+
+    public void AddText(string _text)
+    {
+        if (text == null)
+        {
+            text.Add(_text);
+            text.Add("");
+        }
+        else
+        {
+            if (text[text.Count - 1].Equals(""))
+            {
+                text[text.Count - 1] = _text;
+            }
+            else
+            {
+                text.Add(_text);
+                text.Add("");
+            }
+        }
+    }
+
+    private void SwitchPage()
+    {
+        bool forward = false;
+
+        //check input
+
+        if (forward)
+        {
+            if (text.Count - 1 != displayPointer + 2)
+            {
+                displayPointer += 2;
+            }
+        }
+        else
+        {
+            if (0 != displayPointer - 2)
+            {
+                displayPointer -= 2;
+            }
+        }
+    }
+}
