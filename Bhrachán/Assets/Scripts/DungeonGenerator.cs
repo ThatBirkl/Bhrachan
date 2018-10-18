d76a1897511d5215f40c7a2544a695b00b750e36
ThatBirkl <lkbirkl@googlemail.com>
2018-10-18 11:18:17 +0200

    Update DungeonGenerator.cs
    Updated the formular for the calculation

--- Bhrachán/Assets/Scripts/DungeonGenerator.cs
+++ Bhrachán/Assets/Scripts/DungeonGenerator.cs
@@ -203,7 +203,9 @@ public class DungeonGenerator// : MonoBehaviour
             bool stop = false;
             List<Vector2> riverMarkersTemp = new List<Vector2>();
             int loopCounter = 1;
-            int stopper = 8;//Mathf.FloorToInt(6 * 
+            //Maybe change the /2 to something smaller
+            //2^stopper ~ 100 für map(100, 100)
+            int stopper = Mathf.Sqrt(Mathf.Pow(start.x - end.x, 2) + Mathf.Pow(start.y - end.y, 2)) / 20;//Mathf.FloorToInt(6 * 
                 //Mathf.Sqrt(Mathf.Sqrt(Mathf.Pow(Mathf.Abs(start.x - end.x), 2) + Mathf.Pow(Mathf.Abs(start.y - end.y), 2))) / 10 * 
                 //Mathf.Sqrt(Mathf.Sqrt(dimensions.x * dimensions.y)) / 10);
 
